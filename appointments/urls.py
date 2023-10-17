359bca29591f0df67305e801996bcb49ba16fa3d
alexflip1990 <>
2023-10-17 16:10:35 +0000

    Connected up urls and views for appointment page

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,7 +1,12 @@
 from . import views
 from django.urls import path
 
-urlpatterns = []
-    #     path('', views.PostList.as_view(), name='home'),
-    #     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    #     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+urlpatterns = [
+    path('<pk>/appointments/', views.AppointmentCreate.as_view(),
+         name='appointment_create'),
+]
+# create ApointmentCreateView, referring to PostDetail url below
+
+#     path('', views.PostList.as_view(), name='home'),
+#     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+#     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
--- appointments/views.py
+++ appointments/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import View
+from .models import Appointment
+# import Appointment model
+
+# create AppointmentCreateView
 
 
-# Create your views here.
+class AppointmentCreate(View):
+
+    template_name = 'appointments.html'
+
+    def get_context_data(self, slug, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['appointments'] = self.request.user.appointment_set.all()
+        return context
+
+# 'referring to PostDetail
+# def get()
+#   render that template you created
