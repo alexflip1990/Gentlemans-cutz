d7d18d3c82cfb4a8b3b5df5c673d07931006c961
alexflip1990 <>
2023-10-27 21:45:18 +0000

    Create view code for appointment create class view

--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views.generic import View
 from .models import Appointment
+from datetime import datetime, timedelta
+from django.contrib import messages
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 # import Appointment model
@@ -10,22 +12,36 @@ from django.http import HttpResponseRedirect
 
 class AppointmentCreate(View):
 
+    def get(self, request, *args, **kwargs):
+        appointment = None
+        return render(request, "appointments.html", {"appointment": appointment})
+
+    def post(self, request, *args, **kwargs):
+        service = request.POST.get('service')
+        day = request.POST.get('day')
+        #Storing day and service in django session
+        request.session['service'] = service
+        request.session['day'] = day
+
+        appointment = Appointment(service=service, day=day)
+        appointment.save()
+
+
+
+
+
+
     # template_name = 'appointments.html'
 
-    # def get_context_data(self, slug, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['appointments'] = self.request.user.appointment_set.all()
-    #     return HttpResponseRedirect(reverse('appointments', args=[slug]))
 
-    def get(self, request, *args, **kwargs):
-        # queryset = Appointment.objects.filter()
-        # appointment = get_object_or_404(queryset)
-        appointment = """<h1>welcome to my page</h1>"""
-        return render(
-            request,
-            "appointments.html",
-            {"appointment": appointment})
 
-# 'referring to PostDetail
-# def get()
-#   render that template you created
+    # def get(self, request, *args, **kwargs):
+    #     # queryset = Appointment.objects.filter()
+    #     # appointment = get_object_or_404(queryset)
+    #     appointment = """<h1>welcome to my page</h1>"""
+    #     return render(
+    #         request,
+    #         "appointments.html",
+    #         {"appointment": appointment})
+
+
